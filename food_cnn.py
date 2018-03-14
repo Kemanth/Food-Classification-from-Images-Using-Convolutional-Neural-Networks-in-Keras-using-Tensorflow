e1234d3c3b7f14e6f235283683359bdc217bea8d
Kemanth <kemanth.raj@gmail.com>
2018-03-15 01:24:10 +0530

    added code to save the model and its weights

--- food_cnn.py
+++ food_cnn.py
@@ -8,7 +8,7 @@ from keras.preprocessing.image import ImageDataGenerator
 #Initialize the CNN
 classifier = Sequential()
 #Convolution and Max pooling
-classifier.add(Conv2D(32, (3, 3), input_shape = (64, 64, 3), activation = 'relu'))
+classifier.add(Conv2D(32, (3, 3), input_shape = (128, 128, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2,2)))
 classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2,2)))
@@ -28,15 +28,21 @@ classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metric
 #Fitting CNN to the images
 train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.2, zoom_range=0.2, horizontal_flip=True) 
 test_datagen = ImageDataGenerator(rescale=1./255)
-training_set = train_datagen.flow_from_directory('./dataset/training_set', target_size=(64, 64), batch_size=32, class_mode='binary')
-test_set = test_datagen.flow_from_directory('./dataset/test_set', target_size=(64, 64), batch_size=32, class_mode='binary')
-classifier.fit_generator(training_set, steps_per_epoch=800/32, epochs=25, validation_data=test_set, validation_steps = 200/32)
+training_set = train_datagen.flow_from_directory('./dataset/training_set', target_size=(128, 128), batch_size=32, class_mode='categorical')
+test_set = test_datagen.flow_from_directory('./dataset/test_set', target_size=(128, 128), batch_size=32, class_mode='categorical')
+classifier.fit_generator(training_set, steps_per_epoch=800/32, epochs=50, validation_data=test_set, validation_steps = 200/32)
  
 #Prediction on a new picture
 from keras.preprocessing import image as image_utils
 import numpy as np
-test_image = image_utils.load_img('dataset/new_images/test_dog.jpg', target_size=(64, 64))
+test_image = image_utils.load_img('dataset/test2.jpg', target_size=(128, 128))
 test_image = image_utils.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis=0)
  
 result = classifier.predict_on_batch(test_image)
+if result[0][0] == 1:
+    print('french fries')
+elif result[0][1] == 1:
+    print('pizza')
+elif result[0][2] == 1:
+    print('samosa')
